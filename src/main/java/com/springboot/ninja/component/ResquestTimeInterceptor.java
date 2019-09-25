4c55bf38241079610ecc5189992bb4a8f764e608
fjuribe <fjuribeui@gmail.com>
2019-09-25 20:18:14 -0300

    Guardando logs de peticiones en base de datos

--- src/main/java/com/springboot/ninja/component/ResquestTimeInterceptor.java
+++ src/main/java/com/springboot/ninja/component/ResquestTimeInterceptor.java
@@ -1,16 +1,27 @@
 package com.springboot.ninja.component;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.springboot.ninja.repository.LogRepository;
 
 @Component("requestTimeInterception")
 public class ResquestTimeInterceptor extends HandlerInterceptorAdapter{
 
+	@Autowired
+	@Qualifier("logRepository")
+	private LogRepository logRepository;
 	
 	private static final Log LOG=LogFactory.getLog(ResquestTimeInterceptor.class);
 	
@@ -28,6 +39,19 @@ public class ResquestTimeInterceptor extends HandlerInterceptorAdapter{
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
 		long startTime=(long)request.getAttribute("startTime");
+		
+		String url=request.getRequestURI().toString();
+		Authentication auth=SecurityContextHolder.getContext().getAuthentication();
+		
+		String username="";
+		if(null!=auth && auth.isAuthenticated()) {
+			username=auth.getName();
+		}
+		
+		
+		com.springboot.ninja.entity.Log log=new com.springboot.ninja.entity.Log(new Date(),auth.getDetails().toString(),username,url);
+	   logRepository.save(log);
+	   
 		LOG.info("Url to:'"+request.getRequestURI().toString()+"'in'"+(System.currentTimeMillis()-startTime)+"ms'");
 	}
 
---
+++ src/main/java/com/springboot/ninja/repository/LogRepository.java
@@ -0,0 +1,13 @@
+package com.springboot.ninja.repository;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springboot.ninja.entity.Log;
+
+@Repository("logRepository")
+public interface LogRepository extends JpaRepository <Log,Serializable>{
+
+}
