22d5c7daabd6496b3bdac2f512cd25a7c02c6904
fjuribe <fjuribeui@gmail.com>
2019-09-25 21:42:21 -0300

    Agregando procedimiento automatico por lotes con spring batch

--- src/main/java/com/springboot/ninja/SpringBootNinjaApplication.java
+++ src/main/java/com/springboot/ninja/SpringBootNinjaApplication.java
@@ -2,8 +2,10 @@ package com.springboot.ninja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringBootNinjaApplication {
 
 	public static void main(String[] args) {
